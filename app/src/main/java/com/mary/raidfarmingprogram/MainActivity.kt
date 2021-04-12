f994262f816393ada0b9ee15781891e5f32909fd
Mary <littlemary1379@gmail.com>
2021-04-12 09:16:25 +0900

    activity result

--- app/src/main/java/com/mary/raidfarmingprogram/MainActivity.kt
+++ app/src/main/java/com/mary/raidfarmingprogram/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.mary.raidfarmingprogram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.mary.raidfarmingprogram.constant.Constant
 import com.mary.raidfarmingprogram.constant.SensitiveInfo
 import com.mary.raidfarmingprogram.util.ViewUtil
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val logInRequestCode = 10
+
+    }
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,11 +36,34 @@ class MainActivity : AppCompatActivity() {
                 .requestEmail()
                 .build()
 
-        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     override fun onStart() {
-        var account : GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+        var account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
         super.onStart()
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == logInRequestCode) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+
+                val account = task.getResult(ApiException :: class.java)!!
+
+            } catch (e: Exception) {
+
+            }
+        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, logInRequestCode)
+    }
+
+
 }

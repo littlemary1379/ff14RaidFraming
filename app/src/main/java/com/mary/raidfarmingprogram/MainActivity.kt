d4f98ffaf94f806fc3e64811f1e3a2747e6b4ea7
Mary <littlemary1379@gmail.com>
2021-04-29 08:43:39 +0900

    firebase auth connect

--- app/src/main/java/com/mary/raidfarmingprogram/MainActivity.kt
+++ app/src/main/java/com/mary/raidfarmingprogram/MainActivity.kt
@@ -8,9 +8,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 import com.mary.raidfarmingprogram.constant.Constant
 import com.mary.raidfarmingprogram.constant.SensitiveInfo
+import com.mary.raidfarmingprogram.util.DlogUtil
 import com.mary.raidfarmingprogram.util.ViewUtil
 import java.lang.Exception
 
@@ -19,10 +25,12 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
         private const val logInRequestCode = 10
-
     }
 
+    private lateinit var signInButton: SignInButton
+
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +45,10 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+
+        findView()
+        setListener()
     }
 
     override fun onStart() {
@@ -49,20 +61,51 @@ class MainActivity : AppCompatActivity() {
 
         if (requestCode == logInRequestCode) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-
-                val account = task.getResult(ApiException :: class.java)!!
-
-            } catch (e: Exception) {
-
-            }
+            handleSignInResult(task)
         }
 
+    }
+
+    private fun findView() {
+        signInButton = findViewById(R.id.signInButton)
+
+    }
+
+    private fun setListener() {
+        signInButton.setOnClickListener {
+            signIn()
+        }
     }
 
     private fun signIn() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, logInRequestCode)
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account : GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                account.id?.let {
+                    DlogUtil.d(TAG, it)
+                }
+                account.idToken?.let { firebaseAuthWithGoogle(it) }
+
+            }
+        } catch (e: Exception) {
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(token : String) {
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if(it.isSuccessful) {
+                DlogUtil.d(TAG, auth.currentUser)
+            } else {
+                DlogUtil.d(TAG, "error ${it.exception}")
+            }
+        }
     }
 
 
